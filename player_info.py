c96fce5a31d01df045276a03818234b946dca144
aquadrizzt <aquadrizzt@gmail.com>
2018-10-19 19:21:44 -0400

    maeve-nimue update
    added new roles (maeve, nimue) 
    moved reversers to 7+ 
    fixed lover reroll weights to normalize probabilities of certain roles appearing

--- player_info.py
+++ player_info.py
@@ -7,20 +7,23 @@ import sys
 # get_role_descriptions - this is called when information files are generated.
 def get_role_description(role):
 	return {
-		'Tristan'		: 'The person you see is also Good and is aware that you are Good.\n You and Iseult are collectively a valid Assassination target.',
-		'Iseult'		: 'The person you see is also Good and is aware that you are Good.\n You and Tristan are collectively a valid Assassination target.',
-		'Merlin' 		: 'You know which people have Evil roles, but not who has any specific role.\n You are a valid Assassination target.',
+		'Tristan'		: 'The person you see is also Good and is aware that you are Good.\nYou and Iseult are collectively a valid Assassination target.',
+		'Iseult'		: 'The person you see is also Good and is aware that you are Good.\nYou and Tristan are collectively a valid Assassination target.',
+		'Merlin' 		: 'You know which people have Evil roles, but not who has any specific role.\nYou are a valid Assassination target.',
 		'Percival'		: 'You know which people have the Merlin or Morgana roles, but not specifically who has each.',
 		'Lancelot'		: 'You may play Reversal cards while on missions.',
 		'Guinevere'		: 'During each mission for which you are not on the team, you may select one player on the mission team and see the card they played.\n You are a valid Assassination target.',
 		'Arthur'		: 'You know which Good roles are in the game, but not who has any given role.\nIf two missions have Failed, and less than two missions have Succeeded, you may declare as Arthur.\nAfter declaring, your vote on team proposals is counted twice, but you are unable to be on mission teams until the 5th mission.\nAfter declaring, you are immune to any effect that can forcibly change your vote.',
 		'Titania'		: 'You appear as Evil to all players with Evil roles (except Colgrevance).',
+		'Nimue'			: 'You know which Good and Evil roles are in the game, but not who has any given role.\nYou are a valid Assassination target.',
+
 		'Mordred' 		: 'You are hidden from all Good Information roles. \nLike other Evil characters, you know who else is Evil (except Colgrevance).',
 		'Morgana'		: 'You appear like Merlin to Percival. \nLike other Evil characters, you know who else is Evil (except Colgrevance).',
-		'Maelegant'		: 'You may play Reversal cards while on missions. \nLike other Evil characters, you know who else is Evil (except Colgrevance).',
+		'Maelagant'		: 'You may play Reversal cards while on missions. \nLike other Evil characters, you know who else is Evil (except Colgrevance).',
 		'Agravaine'		: 'You must play Fail cards while on missions. \nIf you are on a mission that Succeeds, you may declare as Agravaine to cause it to Fail instead. \nLike other Evil characters, you know who else is Evil (except Colgrevance).',
 		'Colgrevance' 	: 'You know not only who else is Evil, but what role each other Evil player possess. \nEvil players know that there is a Colgrevance, but do not know that it is you.',
 		'Oberon' 		: "Once per round (except the first), during a vote on a proposal, you can secretly change one other player's vote to a vote of your choice. \nLike other Evil characters, you know who else is Evil (except Colgrevance).",
+		'Maeve' 		: "Once per round (except the first), during a vote on a proposal, you can secretly choose to obscure how each player voted on the proposal and instead have only the amount of upvotes and downvotes presented. \nLike other Evil characters, you know who else is Evil (except Colgrevance).",
 	}.get(role,'ERROR: No description available.')
 
 # get_role_information: this is called to populate information files
@@ -39,12 +42,15 @@ def get_role_information(my_player,players):
 		'Arthur'		: ['{}'.format(player.role) for player in players if player.team is 'Good' and player.role is not 'Arthur'],
 		'Guinevere'		: [],
 		'Titania'		: [],
+		'Nimue'			: ['{}'.format(player.role) for player in players if player.role is not 'Nimue'],
+
 		'Mordred'	 	: ['{} is Evil.'.format(player.name) for player in players if (player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance') or player.role is 'Titania'],
 		'Morgana' 		: ['{} is Evil.'.format(player.name) for player in players if player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance' or player.role is 'Titania'],
-		'Maelegant' 	: ['{} is Evil.'.format(player.name) for player in players if player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance' or player.role is 'Titania'],
+		'Maelagant' 	: ['{} is Evil.'.format(player.name) for player in players if player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance' or player.role is 'Titania'],
 		'Agravaine'		: ['{} is Evil.'.format(player.name) for player in players if player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance' or player.role is 'Titania'],
 		'Colgrevance' 	: ['{} is {}'.format(player.name,player.role) for player in players if player.team is 'Evil' and player is not my_player],
 		'Oberon'		: ['{} is Evil.'.format(player.name) for player in players if player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance' or player.role is 'Titania'],
+		'Maeve'			: ['{} is Evil.'.format(player.name) for player in players if player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance' or player.role is 'Titania'],
 	}.get(my_player.role,[])
 
 class Player():
@@ -97,38 +103,60 @@ def get_player_info(player_names):
 	num_good = num_players - num_evil
 
 	# establish available roles
-	good_roles = ['Merlin','Percival','Lancelot','Tristan','Iseult']
-	evil_roles = ['Mordred','Morgana','Maelegant']
+	good_roles = ['Merlin','Percival','Tristan','Iseult']
+	evil_roles = ['Mordred','Morgana','Maeve']
 
-	if num_players == 2:
-		good_roles = ['Titania']
-		evil_roles = ['Agravaine']
-		num_good = 1
-		num_evil = 1
+	# additional roles for player-count
+	# 5 only 
+	if num_players < 6: 
+		good_roles.append('Nimue')
 
-	# roles added in larger games
+	# 7 plus
 	if num_players > 6:
 		good_roles.append('Arthur')
+		good_roles.append('Lancelot')
+		evil_roles.append('Maelagant')
 
+	# 8 plus 
 	if num_players > 7:
 		good_roles.append('Guinevere')
 		evil_roles.append('Agravaine')
 		evil_roles.append('Oberon')
 
+	# 10 only
 	if num_players == 10:
 		evil_roles.append('Colgrevance')
 
+	'''
+	code for testing role interaction
+	if num_players == 2:
+		good_roles = ['Merlin']
+		evil_roles = ['Maeve']
+		num_good = 1
+		num_evil = 1
+	''' 
 	good_roles_in_game = random.sample(good_roles,num_good)
 	evil_roles_in_game = random.sample(evil_roles,num_evil)
 
-	# remove lone lovers
-	if sum(gr in ['Tristan','Iseult'] for gr in good_roles_in_game) == 1:
-		available_roles = set(good_roles)-set(good_roles_in_game)-set(['Tristan','Iseult'])
+	# lone lovers are rerolled
+	# 50% chance to reroll lone lover
+	# 50% chance to reroll another role into a lover
+	if sum(gr in ['Tristan','Iseult'] for gr in good_roles_in_game) == 1 and num_good > 1:
 		if 'Tristan' in good_roles_in_game:
-			good_roles_in_game.remove('Tristan') 
+				good_roles_in_game.remove('Tristan') 
 		if 'Iseult' in good_roles_in_game:
-			good_roles_in_game.remove('Iseult') 
-		good_roles_in_game.append(random.sample(set(available_roles),1)[0])
+				good_roles_in_game.remove('Iseult') 
+
+		if random.choice([True, False]):
+			# replacing the lone lover
+			available_roles = set(good_roles)-set(good_roles_in_game)-set(['Tristan','Iseult'])
+			good_roles_in_game.append(random.sample(set(available_roles),1)[0])
+		else: 
+			# upgrading to pair of lovers
+			rerolled = random.choice(good_roles_in_game)
+			good_roles_in_game.remove(rerolled)
+			good_roles_in_game.append('Tristan')
+			good_roles_in_game.append('Iseult')
 
 	# roles after validation
 	print(good_roles_in_game)
